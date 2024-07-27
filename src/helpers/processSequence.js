0040428ab1d2b107acc8d79f6684458ce04b9c11
liminfinity <108136720+liminfinity@users.noreply.github.com>
2024-07-27 15:14:34 +0700

    feat: complete second task

--- src/helpers/processSequence.js
+++ src/helpers/processSequence.js
@@ -14,38 +14,98 @@
  * Иногда промисы от API будут приходить в состояние rejected, (прямо как и API в реальной жизни)
  * Ответ будет приходить в поле {result}
  */
- import Api from '../tools/api';
+ import { allPass, compose, partialRight, length, gt, lt, partial, andThen, tryCatch, tap, ifElse } from 'ramda';
+import Api from '../tools/api';
 
- const api = new Api();
 
- /**
-  * Я – пример, удали меня
-  */
- const wait = time => new Promise(resolve => {
-     setTimeout(resolve, time);
- })
+const api = new Api();
+
+const lt10 = partialRight(lt, [10])
+
+const gt2 = partialRight(gt, [2]);
+
+const gt0 = partialRight(gt, [0]);
+
+const lengthLt10 = compose(lt10, length);
+
+const lengthGt2 = compose(gt2, length);
+
+const isPositive = compose(gt0, parseFloat);
+
+const isDecimal = str => /^\d+(?:\.\d+)?$/.test(str);
+
+const getConvertedNumber = api.get('https://api.tech/numbers/base');
+
+const round = Math.round;
+
+const convertToBinary = async (number) => {
+    const res = await getConvertedNumber({ from: 10, to: 2, number: number.toString() })
+    return res.result;
+};
+
+const getAnimal = async (animalId) => {
+    const res = await api.get(`https://animals.tech/${animalId}`)(undefined);
+    return res.result;
+};
+
+const square = number => number ** 2
+
+const mod3 = number => number % 3
+
+const isValidNumber = allPass([
+    lengthLt10,
+    lengthGt2,
+    isPositive,
+    isDecimal
+])
 
  const processSequence = ({value, writeLog, handleSuccess, handleError}) => {
-     /**
-      * Я – пример, удали меня
-      */
-     writeLog(value);
+    const handleValidationError = partial(handleError, ['ValidationError'])
 
-     api.get('https://api.tech/numbers/base', {from: 2, to: 10, number: '01011010101'}).then(({result}) => {
-         writeLog(result);
-     });
+    const process = compose(
+        andThen(compose(
+            andThen(handleSuccess),
+            getAnimal,
+            tap(writeLog),
+            mod3,
+            tap(writeLog),
+            square,
+            tap(writeLog),
+            length,
+            tap(writeLog),
+        )),
+        convertToBinary,
+    );
+    
+    const handleValidationSuccess = compose(
+        tryCatch(process, handleError),
+        tap(writeLog),
+        round,
+        parseFloat,
+        tap(writeLog),
+    );
+    
+    ifElse(isValidNumber, handleValidationSuccess, handleValidationError)(value);
 
-     wait(2500).then(() => {
-         writeLog('SecondLog')
 
-         return wait(1500);
-     }).then(() => {
-         writeLog('ThirdLog');
-
-         return wait(400);
-     }).then(() => {
-         handleSuccess('Done');
-     });
  }
 
 export default processSequence;
+
+    //  writeLog(value);
+
+    //  api.get('https://api.tech/numbers/base', {from: 2, to: 10, number: '01011010101'}).then(({result}) => {
+    //      writeLog(result);
+    //  });
+
+    //  wait(2500).then(() => {
+    //      writeLog('SecondLog')
+
+    //      return wait(1500);
+    //  }).then(() => {
+    //      writeLog('ThirdLog');
+
+    //      return wait(400);
+    //  }).then(() => {
+    //      handleSuccess('Done');
+    //  });
